383ebbfc5f60750eb30b72da59004d58c78676e5
vkings1 <elmer.alluad@gmail.com>
2020-04-16 00:35:01 +0800

    initail added

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto my-4 my-lg-0">
           <li class="nav-item ">
-            <a class="nav-link js-scroll-trigger" href="#about">About</a>
+            <a class="nav-link js-scroll-trigger" href="#about"> About </a>
           </li>
           <li class="nav-item">
             <a class="nav-link js-scroll-trigger" href="#services">Services</a>
